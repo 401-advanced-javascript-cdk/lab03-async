fbabb75139a867194c96c160978cac4cdd53975e
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-15 16:25:33 -0700

    added promises and async/await files and tests

--- edit-file.js
+++ edit-file.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const faker = require('faker');
 
 const args = process.argv.slice(2);
 
@@ -8,5 +9,12 @@ console.log({args});
 
 fs.readFile(args[0], 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data);
+  console.log('Contents of test.txt: ', data);
+  fs.writeFile(args[0], `${data}, ${faker.random.word()}`, (err) => {
+    if (err) throw err;
+    fs.readFile(args[0], 'utf8', (err, data) => {
+      if (err) throw err;
+      console.log('Contents of test.txt: ', data);
+    });
+  });
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const fileReader = require('./lib/reader.js');
+const fileReaderPromises = require('./lib/reader-promises.js');
+const fileReaderAsync = require('./lib/reader-async.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
@@ -10,6 +12,13 @@ if( ! (files instanceof Array && files.length) ) {
 }
 
 fileReader(files, (err,data) => {
-  if ( err ) { throw err; }
-  console.log('From Callback:', data);
+  if ( err ) { 
+    console.warn('File(s) not found');
+    return; 
+  }
+  console.log('From Callbacks: ', data);
 });
+
+fileReaderPromises(files);
+
+fileReaderAsync(files);
